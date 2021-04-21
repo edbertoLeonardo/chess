4d3182c4890b597c5cd5422427eb3658b1131fb6
edbertoLeonardo <edbleonardo@gmail.com>
2021-04-21 18:33:13 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import exception.ChessException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		while(true) {
+		try {
+			UI.clearScreen();
+			UI.printMatch(chessMatch, captured);
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(scanner);
+			
+			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces(), possibleMoves);
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(scanner);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+			if(capturedPiece != null) {
+				captured.add(capturedPiece);
+			}
+			
+			}catch (ChessException e) {
+				System.out.println(e.getMessage());
+				scanner.nextLine();
+			}catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				scanner.hasNextLine();
+			}
+		}
+		
+	}
+
+}
